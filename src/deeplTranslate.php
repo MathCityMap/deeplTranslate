4edbde50ea9a31326db7e758ab1365ca45ff89ec
Damian Scheerer <damian.scheerer@autentek.de>
2025-04-15 16:24:16 +0200

    update deeplTranslate to propagate error messages given by deeplApi correctly

--- src/deeplTranslate.php
+++ src/deeplTranslate.php
@@ -29,6 +29,29 @@ class deeplTranslate
 
         $context = stream_context_create($context_options);
         $resp = file_get_contents($this->rootUrl . "v2/translate", false, $context);
+        if ($resp === false) {
+           $headers = $this->parseHeaders($http_response_header);
+           $resp = ["error" => true, "response_code" => $headers["response_code"]];
+           return $resp;
+        }
         return json_decode($resp);
+    }
+
+    private function parseHeaders( $headers )
+    {
+        $head = array();
+        foreach( $headers as $k=>$v )
+        {
+            $t = explode( ':', $v, 2 );
+            if( isset( $t[1] ) )
+                $head[ trim($t[0]) ] = trim( $t[1] );
+            else
+            {
+                $head[] = $v;
+                if( preg_match( "#HTTP/[0-9\.]+\s+([0-9]+)#",$v, $out ) )
+                    $head['reponse_code'] = intval($out[1]);
+            }
+        }
+        return $head;
     }
 }
